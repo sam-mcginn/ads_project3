122c29b30708549866853fb421c1cc18add6ceeb
smcginn-adsd <smcginn-adsd@ADSD-Ubuntu.myguest.virtualbox.org>
2023-12-07 21:06:08 -0500

    component connections update

--- ads_project3.vhd
+++ ads_project3.vhd
@@ -36,8 +36,11 @@ architecture top_level of ads_project3 is
 	signal head_ptr_gray: pointer;		-- pointer from sync to consumer FSM
 	signal tail_ptr_gray: pointer;		-- pointer from sync to producer FSM
 	
+	signal write_enable: std_logic_vector;
 	
 	-- FIX: CONVERT ADC DATA OUT (NATURAL) --> BUFFER DATA IN (STD_LOGIC_VECTOR)
+	signal buffer_data_in: std_logic_vector((data_width-1) downto 0);
+	signal buffer_data_out: std_logic_vector((data_width-1) downto 0);
 	
 begin
 	-- Buffer RAM; side A = producer/head side, B = consumer/tail side
@@ -51,28 +54,28 @@ begin
 			clk_b		=> base_clock,
 			addr_a 	=> head_ptr, --head pointer
 			addr_b 	=> tail_ptr, -- tail pointer 
-			data_a	: in std_logic_vector((DATA_WIDTH-1) downto 0),
+			data_a	=> buffer_data_in,
 			data_b	=> open,
-			we_a		: in std_logic := '1',
+			we_a		=> write_enable,
 			we_b		=> open,
-			q_a		: out std_logic_vector((DATA_WIDTH -1) downto 0),
-			q_b		=> open
+			q_a		=> open,
+			q_b		=> buffer_data_out
 		);
 		
 	-- PRODUCER SIDE:
 	-- ADC
 	adc_clock_in <= not adc_clock_in after -- FIX - DRIVE FROM PLL (10MHZ)
-	adc_ch_sel => 1;				-- FIX - ????
-	adc_mode => '1';
+	adc_ch_sel <= 1;				-- FIX - ????
+	adc_mode <= '1';
 	adc_driver: max10_adc
 		port (
-			pll_clk 		<= adc_clock_in,				-- clock input (10 MHz)
-			chsel 		<= adc_ch_sel,					-- channel select (5 bit num.)
-			soc			<= adc_soc,						-- start of conversion
-			tsen 			<= adc_mode,					-- Mode, 0=normal, 1=temp-sensing
-			dout:			<= adc_data,					-- dout:	data output
-			eoc 			<= adc_eoc,						-- end of conversion
-			clk_dft 		<= prod_clock					-- clk_dft: clock output from clock divider
+			pll_clk 		=> adc_clock_in,				-- clock input (10 MHz)
+			chsel 		=> adc_ch_sel,					-- channel select (5 bit num.)
+			soc			=> adc_soc,						-- start of conversion
+			tsen 			=> adc_mode,					-- Mode, 0=normal, 1=temp-sensing
+			dout:			=> adc_data,					-- dout:	data output
+			eoc 			=> adc_eoc,						-- end of conversion
+			clk_dft 		=> prod_clock					-- clk_dft: clock output from clock divider
 		);
 
 	-- Producer side FSM
@@ -81,14 +84,14 @@ begin
 			ucode_rom => producer_ucode
 		)
 		port (
-			clock:				in std_logic,
-			reset => reset,
-			external_ctl_1:	in	std_logic;		-- end of conversion indicator
+			clock					=> prod_clock,
+			reset 				=> reset,
+			external_ctl_1 	=> adc_eoc,	-- end of conversion indicator
 			external_ctl_2:	in	std_logic;		-- can advance indicator
 
-			driver_1:			out std_logic;		-- drives start conversion signal
+			driver_1				=> adc_soc,		-- drives start conversion signal
 			driver_2:			out std_logic;		-- drives advance head/tail signal
-			driver_3:			out std_logic		-- drives write enable
+			driver_3				=> write_enable		-- drives write enable
 		);
 		
 	-- Binary to gray code converter (consumer domain)
@@ -155,11 +158,12 @@ begin
 
 
 	-- PROCESSES, etc.:
-	-- Drive ADC to sample temperature at 10MHz
-
-	-- Use output clock from ADC to drive logic related to this (producer) domain
-
-
+	-- Convert ADC data out (natural) --> buffer data in (std_logic_vector
+	
+	-- Get next pointer:
+	
 	-- Convert index pointer of clock domain into gray code before transmitting it (?)
+	
 	-- Update 7 segment displays at 50MHz
+	
 end architecture top_level;
